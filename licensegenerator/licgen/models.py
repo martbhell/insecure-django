8c019d24df51f04c7b6976c784d0df4fb46e4ffa
Johan Guldmyr <martbhell@protonmail.com>
2021-02-21 19:35:03 +0200

    DB Schema
    Table/Model Profile
    	(username, social security number, password, num_licenses, admin)
    
    Table/Model Licenses
    	(licenseid, owner, mac_address, created_at, expired_at)
    
    https://docs.djangoproject.com/en/3.1/intro/tutorial02/
    
    https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html

--- licensegenerator/licgen/models.py
+++ licensegenerator/licgen/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
+# TODO: Define signals to create/update Profile  ?
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    social_security = models.CharField(max_length=13)
+    num_licenses = models.IntegerField(default=10)
+
+class License(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    licenseid = models.CharField(max_length=32)
+    mac_address = models.CharField(max_length=18)
+    created_at = models.DateTimeField('creation date', auto_now_add=True)
+    # TODO: autoadd now + 1y ? Or do this in code?
+    expire_at = models.DateTimeField('expiration date')
