4c546a26ea87dbec312c8c3648ad16b6384eec5f
Johan Guldmyr <martbhell@protonmail.com>
2021-02-23 12:03:48 +0200

    Create users and profiles

--- licensegenerator/licgen/models.py
+++ licensegenerator/licgen/models.py
@@ -8,12 +8,12 @@ from django.contrib.auth.models import User
 # TODO: Define signals to create/update Profile  ?
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    social_security = models.CharField(max_length=13)
+    social_security = models.CharField(max_length=13, unique=True)
     num_licenses = models.IntegerField(default=10)
 
 class License(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    licenseid = models.CharField(max_length=32)
+    licenseid = models.CharField(max_length=32, unique=True)
     mac_address = models.CharField(max_length=18)
     created_at = models.DateTimeField('creation date', auto_now_add=True)
     # TODO: autoadd now + 1y ? Or do this in code?
--- licensegenerator/licgen/views.py
+++ licensegenerator/licgen/views.py
@@ -18,24 +18,33 @@ def LicensesView(request):
     return render(request, 'index.html', {'licenses': [ { "licenseid": "123ABC", "owner": "bob", "created_at": "2021-12-13", "expired_at": "2022-12-13"} ] } )
 
 def CreateUsers(request):
-    print(User.objects.get(username='bob').__dict__)
-    print(Profile.objects.all())
-    print(User.objects.get(username='bob'))
+#    print(User.objects.get(username='bob').__dict__)
+#    print(Profile.objects.all())
+#    print(User.objects.get(username='bob'))
 
     try:
-        print("We already have a user called %s" % User.objects.get(username='bob'))
-#        print("We already have a Profile called %s" % Profile.objects.get(username='bob'))
-    except:
         bob = User.objects.create_user(username='bob', password='squarepants')
+    except:
+        bob = User.objects.get(username='bob')
+        print("We already have a user called %s" % bob)
+
+    bobid = User.objects.get(username='bob').id
+    try:
+        bobpro = Profile.objects.create(user=bob, social_security="20010603-1234", num_licenses=2)
+    except:
+        bobpro = Profile.objects.get(user=bob)
+        print("We already have a Profile called %s with SS %s" % (bob, bobpro.social_security))
 
     try:
-        print("We already have a Profile called %s" % Profile.objects.get(username='bob'))
-    except:
-        bob = Profile.objects.create_user(username='bob', password='squarepants')
-
-    try:
-        print("We already have a user called %s" % User.objects.get(username='alice'))
-    except:
         alice = User.objects.create_user(username='alice', password='redqueen')
+    except:
+        alice = User.objects.get(username='alice')
+        print("We already have a user called %s" % alice)
+
+    aliceid = User.objects.get(username='alice').id
+    try:
+        alicepro = Profile.objects.create(user=alice, social_security="20010603-1234", num_licenses=2)
+    except:
+        print("We already have a Profile called %s" % alice)
 
     return HttpResponse('OK: Users Created')
