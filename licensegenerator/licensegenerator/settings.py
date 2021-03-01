f3f339693e0fae8c36efbc601998d33818ff4fee
Johan Guldmyr <martbhell@protonmail.com>
2021-03-01 10:20:34 +0200

    FLAW 4.2: SECRET_KEY stored in GitHub
    As an example, we modify settings.py to allow reading a 'secrets.key' file.
    
    The current choice of location for secrets.key is likely not the best.
    
    In production I would put this in a place that fits with how the
    application is deployed.

--- licensegenerator/licensegenerator/settings.py
+++ licensegenerator/licensegenerator/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '03!mpyjyv7y!los8hi&g2t7!-h(f^xyv%j(l9a&hm25j=1ns#o'
+try:
+    f = open("secret.key", "r")
+    SECRET_KEY = f
+except:
+    SECRET_KEY = '03!mpyjyv7y!los8hi&g2t7!-h(f^xyv%j(l9a&hm25j=1ns#o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
